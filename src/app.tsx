b1cd16821ec82ce94712054566b7f78a24edfbbd
Pablo Rossoni <pablorossoni107@outlook.com>
2024-02-07 13:58:11 -0300

    Passando components

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import logo from './assets/logo.svg'
+import { NewNorteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
 export function App() {
@@ -7,8 +8,8 @@ export function App() {
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
-        <input 
-          type="text" 
+        <input
+          type="text"
           placeholder='Busque em suas notas...'
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
         />
@@ -17,19 +18,24 @@ export function App() {
       <div className="h-px bg-slate-700" />
 
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
-        <div className="rounded-md bg-slate-700 p-5 space-y-3">
-          <span className="text-sm font-medium text-slate-200">
-            Adicionar nota
-          </span>
-          <p className="text-sm leading-6 text-slate-400">
-            Grave uma nota em áudio que será convertida para texto automaticamente.
-          </p>
-        </div>
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
+        <NewNorteCard />
+
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }} />
+
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }} />
+
+        <NoteCard note={{
+          date: new Date(),
+          content: 'Hello World'
+        }} />
+
       </div>
     </div>
 
